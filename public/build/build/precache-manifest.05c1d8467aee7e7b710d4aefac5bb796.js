fa10e10f15524dbd61378193b835a12725686993
Wei Chen <wchen8878@gmail.com>
2019-04-08 21:12:19 -0400

    deploy the application to the firebase.

---
+++ public/build/build/precache-manifest.05c1d8467aee7e7b710d4aefac5bb796.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bc8c19e2ec29d26692c",
+    "url": "/static/css/main.f4ab3aa9.chunk.css"
+  },
+  {
+    "revision": "4bc8c19e2ec29d26692c",
+    "url": "/static/js/main.d875c23b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7cf6760385da5c6817e0",
+    "url": "/static/css/2.e0f04f9d.chunk.css"
+  },
+  {
+    "revision": "7cf6760385da5c6817e0",
+    "url": "/static/js/2.deebb49b.chunk.js"
+  },
+  {
+    "revision": "0ee480ea8467608f65c2466d84c73657",
+    "url": "/index.html"
+  }
+];
