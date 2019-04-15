305de90f64d71650b3df1afd0584372bd693c1e3
Wei Chen <wchen8878@gmail.com>
2019-04-15 17:47:37 -0400

    updata the card fountion into accounting page

--- src/components/forms/AccountingPage.js
+++ src/components/forms/AccountingPage.js
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
-import { Form,Table, Badge, Menu, Dropdown, Input, } from 'antd';
+import { Form,Table, Input, Row, Card, Icon, Typography } from 'antd';
 import { FirebaseContext } from 'Component/Firebase';
+const { Text } = Typography;
 
 
 export class AccountingPage extends React.Component {
@@ -9,25 +10,27 @@ export class AccountingPage extends React.Component {
     super(props);
     this.state = {
       company: [],
-      employeeList: [],
       payroll: null,
     };
   }
 
   componentDidMount = () => {
-    console.log('did mount');
     this.props.firebase.companies()
     .onSnapshot((doc) => {
       console.log(doc.data());
       this.setState({
         payroll: doc.data(),
       });
+      this.setState({
+        company: doc.data().companyInfo,
+      });
+      console.log(doc.data().companyInfo);
     });
+
   }
   
 
   render() {
-    console.log('rendering...');
     const { payroll } = this.state;
     const monthList = [];
     if (payroll) {
@@ -39,21 +42,53 @@ export class AccountingPage extends React.Component {
       { title: '底薪', dataIndex: 'baseSalary', key: 'baseSalary' },
       { title: '小费', dataIndex: 'baseTips', key: 'baseTips' },
     ];
-
+    
     return (
-      payroll ? monthList.map((eachMonth) => (
+      <div>
         <div>
-          <span>{eachMonth}</span>
-          <Table
-            key={eachMonth}
-            className="components-table-demo-nested"
-            columns={columns}
-            dataSource={payroll.monthPayroll[eachMonth]}
-            size="small"
-            pagination={false}
-          />
+          <Row type="flex" justify="center">
+            <Card
+              size="small"
+              title="Boiling Crab and Crawfish Inc"
+              style={{ width: 350, marginBottom: 20, textAlign: 'center' }}
+              >
+                <p>
+                    <Text disabled><Icon type="bank" /></Text> <Text strong>{this.state.company.DBA}</Text>
+                </p>
+                <p>    
+                    <Text disabled><Icon type="money-collect" /></Text> <Text strong>{this.state.company.companyName}</Text>
+                </p>
+                <p>
+                    <Text disabled><Icon type="contacts" /></Text><Text strong>{this.state.company.Address}</Text>
+                </p> 
+                <p> 
+                    <Text disabled><Icon type="phone" /></Text> <Text strong>{this.state.company.contact}</Text>
+                </p> 
+                <p> 
+                    <Text disabled><Icon type="wechat" /></Text> <Text strong>{this.state.company.wechat}</Text>
+                </p>
+            </Card>
+          </Row>
         </div>
-      )) : null
+        <div>
+          {payroll ? monthList.map((eachMonth) => (
+            <div>
+              <span>{eachMonth}</span>
+              <Table
+                key={eachMonth}
+                className="components-table-demo-nested"
+                columns={columns}
+                dataSource={payroll.monthPayroll[eachMonth]}
+                size="small"
+                pagination={false}
+              />
+            </div>
+          )) : null}
+        </div>
+      </div>
+      
+
+
     );
   }
 }
